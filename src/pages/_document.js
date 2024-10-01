323d1dd56dfb2e14f46ade8c3f41c43fadb4580f
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2024-10-01 18:28:48 +0000

    Aula dia 01-10

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,15 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossorigin></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"
+          crossorigin></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
+          crossorigin></script>
+
+        <script>var Alert = ReactBootstrap.Alert;</script>
       </body>
     </Html>
   );
