94b976dd60299bd5603841554df59afce9eea9dd
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2024-10-17 19:58:39 +0000

    aula 15-10, 2

--- src/pages/components/footer.js
+++ src/pages/components/footer.js
@@ -2,8 +2,8 @@ import {Container} from "react-bootstrap"
 import style from "@/styles/Footer.module.css"
 export default function Footer() {
     return <>
-    <Container className={`${style.degradeFooter} position-button bottom-0 start-50`}>
-        <footer className="px-3 text-dark rounded">
+    <Container>
+        <footer className={`${style.degradeFooter} position-button bottom-0 start-50 px-3 text-dark rounded border border-2 border-primary`}>
             <ul className="nav justify-content-center border-bottom pb-1 mb-2 ">
                 <li className="nav-item">Disciplina de FrameWorks2</li>
             </ul>
--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -4,13 +4,14 @@ import style from "@/styles/Navbar.module.css"
 export default function Menu() {
   return (
   <Container>
-    <Navbar expand="sm" bg="dark" className={`${style.degrademenu} p-2 border border-2 border-warning rounded-2`}>
+    <Navbar expand="sm" bg="dark" className={`${style.degrademenu} p-2 border border-2 border-primary rounded-2`}>
         <Navbar.Brand href="/">6V</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="/lista">Lista</Nav.Link>
           </Nav>
         </Navbar.Collapse>
     </Navbar>
