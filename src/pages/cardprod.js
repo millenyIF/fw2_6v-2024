1c3e72b71b0eafb5c1c52309e44cf949f4b2d2db
millenyIF <milleny.virissimo@estudante.ifms.edu.br>
2024-12-03 18:13:32 +0000

    cardProdutos

--- src/pages/cardprod.js
+++ src/pages/cardprod.js
@@ -1,7 +1,7 @@
 export default function CardProd(props) {
     return <>
         <div className="col-sm-6 mb-3 mb-sm-0">
-            <div className="card">
+            <div className="card" key={props.id}>
                 <div className="card-body">
                     <h5 className="card-title">{props.titulo}</h5>
                     <p className="card-text">{props.descricao}</p>
--- src/pages/pesportes.js
+++ src/pages/pesportes.js
@@ -2,7 +2,7 @@ import { Container } from "react-bootstrap";
 import Menu from "./components/navbar";
 import Footer from "./components/footer";
 import CardProd from "./cardprod";
-export default function Pesportes() {
+export default function Pesportes({produtos}) {
     return <>
         <Menu />
         <Container className="border border-2 
@@ -11,12 +11,19 @@ export default function Pesportes() {
         </Container>
         <Container>
             <div className="row">
-                <CardProd titulo={"Bola de Voley"}
-                    descricao={"Bola de voley"} preco={99.90} />
-                <CardProd titulo={"Bola de Futebol"} descricao={"Bola de Futebol"} preco={89.90} />
-                <CardProd titulo={"Bola de Handball"} descricao={"Bola de Handball"} preco={79.90} />
+                {produtos.map(produto=>
+                    <CardProd titulo={produto.titulo}
+                    descricao={produto.descricao}
+                    preco={produto.preco}
+                    key={produto.id}/>
+                )}
             </div>
         </Container>
         <Footer />
     </>
+}
+export async function getServerSideProps(){
+    const res = await fetch('http://localhost:3000/api/produtos/data')
+    const repo = await res.json()
+    return { props: {produtos: repo}}
 }
